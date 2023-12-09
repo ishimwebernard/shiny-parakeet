ff4688872813e001f6a5768b80af72f65147fdcc
ishimwebernard <isbernard2001@gmail.com>
2023-12-09 20:26:45 +0200

    mend

--- src/components/tab.jsx
+++ src/components/tab.jsx
@@ -64,6 +64,7 @@ const Nav = () => {
     <small class="self-center text-sm font-thin tet-center leading-tight">Designs Tracking and Correction System</small>
 </a>
 <p>{userInformation.Username}</p>
+<p>Designer</p>
 <div class="flex md:order-2">
   <a href="/signin">
   <button type="button" class="text-white bg-green-500 hover:bg-green-dark focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">Log Out</button>
